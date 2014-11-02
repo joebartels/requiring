f4352028730e5259839465ea17276e14c6582a94
joseph bartels <joebartels@hotmail.com>
2014-11-02 01:17:52 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,112 @@
+'use strict';
+var fs    = require('fs');
+var path  = require('path');
+
+/*
+  If no file extension exists, it creates an array of possible files:
+  filePath, filePath.js, filePath.json, filePath.node
+  @method possiblePaths
+  @private
+  @param {String} mod
+  @return {String}
+  @default {String}
+*/
+function buildPath(mod) {
+  mod = mod || './';
+  var dir = process.cwd();
+  var fullPath = path.join(dir, mod);
+  return fullPath;
+}
+
+/*
+  If no file extension exists, it creates an array of possible files:
+  filePath, filePath.js, filePath.json, filePath.node
+  @method possiblePaths
+  @private
+  @param {String} filePath
+  @return {Array} the path
+  @default []
+*/
+function possiblePaths(filePath) {
+  if (!path.extname(filePath)) {
+    return [
+        filePath,
+        filePath+'.js',
+        filePath+'.json',
+        filePath+'.node'
+      ];
+  } else {
+    return [filePath];
+  }
+}
+
+/*
+  attempts to resolve an actual path from an array of possible paths.
+  @method actualPath
+  @private
+  @param {Array} possiblePaths an array of paths to check for validity
+  @param {Mixed} def the default value to return if no path is valid
+  @return {String} the path
+  @default null
+*/
+function actualPath(possiblePaths, def) {
+  possiblePaths = possiblePaths || [];
+  def = 'undefined' !== typeof def ? def : null;
+
+  var pathToUse = def;
+
+  for (var i = 0; i < possiblePaths.length; i++) {
+    if (fs.existsSync(possiblePaths[i])) {
+      pathToUse = possiblePaths[i];
+      break;
+    }
+  }
+  return pathToUse;
+}
+
+/*
+  Returns a module async through the passed callback.
+  ```javascript
+    var requires = require('requires');
+
+    requires.async('./my-cool-file', function(err, mod) {
+      if (err !== null) {
+        mod.method();
+      }
+    });
+  ```
+  @method async
+  @param {String} mod The module to require.
+  @param {Function} callback The callback to use.
+*/
+module.exports.async = function(mod, callback) {
+  mod = mod || '';
+  var fullPath = buildPath(mod);
+  var pathsToTry = possiblePaths(fullPath);
+  var pathToUse = actualPath(pathsToTry, fullPath);
+
+  return fs.realpath(pathToUse, function(err, fullPath) {
+    if (err !== null) {
+      return callback(err);
+    } else {
+      var ret = require(fullPath);
+      return callback(null, ret);
+    }
+  });
+}
+
+/*
+  Returns a module synchronously. Returns undefined if none is found.
+  @method sync
+  @param {String}
+*/
+module.exports.sync = function(mod) {
+  mod = mod || '';
+  var fullPath = buildPath(mod);
+  var pathsToTry = possiblePaths(fullPath);
+  var pathToUse = actualPath(pathsToTry)
+
+  if (pathToUse !== null) {
+    return require(pathToUse);
+  }
+}
---
+++ test.js
@@ -0,0 +1,56 @@
+'use strict';
+var assert = require('assert');
+var requiring = require('./');
+
+it('requiring.sync should return undefined when no module exists', function() {
+  var testModule = requiring.sync('./dontexist');
+
+  assert.deepEqual(testModule, undefined, 'Module not found and returns undefined');
+});
+
+it('requiring.sync should return the module if it exists', function() {
+  var testModule = requiring.sync('./tests/dummy');
+  var ret = testModule();
+
+  assert.deepEqual(typeof testModule, 'function', 'The module is a function');
+  assert.deepEqual(ret, 'I am a dummy', 'The correct module is returned');
+});
+
+it('requiring.sync should return the main file from the package.json', function() {
+  var testModule = requiring.sync('./tests');
+  var ret = testModule();
+
+  assert.deepEqual(typeof testModule, 'function', 'The module is a function');
+  assert.deepEqual(ret, 'I am the main file', 'The correct module is returned');
+});
+
+it('requiring.async should return an error in the callback when no module exists', function(done) {
+  requiring.async('./dontexist', function(err, testModule) {
+
+    assert.notDeepEqual(err, null, 'Callback recieves an error');
+    assert.deepEqual(testModule, undefined, 'Callback does not recieve a module');
+    done();
+  });
+});
+
+it('requiring.async should return a module in the callback when module exists', function(done) {
+  requiring.async('./tests/dummy', function(err, testModule) {
+    var ret = testModule();
+
+    assert.ifError(err);
+    assert.deepEqual(typeof testModule, 'function', 'The module is a function');
+    assert.deepEqual(ret, 'I am a dummy', 'Callback recieves the correct module');
+    done();
+  });
+});
+
+it('requiring.async should return the main file from package.json in the callback', function(done) {
+  requiring.async('./tests', function(err, testModule) {
+    var ret = testModule();
+
+    assert.ifError(err);
+    assert.deepEqual(typeof testModule, 'function', 'The module is a function');
+    assert.deepEqual(ret, 'I am the main file', 'Callback recieves the correct module');
+    done();
+  });
+});
